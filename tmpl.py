05b68f8f85592be702b42a140f05bb056d088834
Buster75 <Thomas.Bergqvist@telia.com>
2016-05-20 08:35:06 +0200

    Update tmpl.py

--- tmpl.py
+++ tmpl.py
@@ -4,7 +4,7 @@ import re, sys, os
 '''
 Created on 25 apr 2016
 
-@author: 66109031
+@author: Thomas Bergqvist
 '''
 os.popen("java -jar /nfshome/66109031/public/skriptstatistik.jar tmpltest")
 maxlenght = 0
